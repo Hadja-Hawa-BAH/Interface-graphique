10961712a5b7285c2ba7c971d077cfda1444bb08
Hadja41 <hadhawabah@gmail.com>
2024-07-18 11:07:55 +0200

    Modification des sliders

--- checkbox/checkbox/src/main/java/fr/afpa/App.java
+++ checkbox/checkbox/src/main/java/fr/afpa/App.java
@@ -8,6 +8,9 @@ import javafx.scene.control.RadioButton;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TitledPane;
+import javafx.scene.control.Toggle;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -15,14 +18,14 @@ import javafx.stage.Stage;
  * JavaFX App
  */
 public class App extends Application {
-
+    
     public static void main(String[] args) {
         launch();
     }
 
     @Override
     public void start(Stage stage) {
-        stage.setTitle("");
+        stage.setTitle("CheckBox - Radiobutton - Slider");
 
         // Création des labels couleurs et les sliders
         Label rouge = new Label("Rouge");
@@ -37,8 +40,10 @@ public class App extends Application {
         // Création du textfield pour la saisie
         TextField textField = new TextField();
         textField.setPromptText("Saisissez votre texte");
+        textField.setPrefSize(300, 10);
 
         Label labelResult = new Label();
+        VBox vBoxText = new VBox(10, textField, labelResult);
         
         VBox vBox = new VBox(10);
 
@@ -51,55 +56,76 @@ public class App extends Application {
         }
 
         TitledPane titledPane = new TitledPane("Paramètres label", vBox);
-        VBox finalVBox = new VBox(10, textField, labelResult, titledPane);
+        HBox hBox1 = new HBox(vBoxText, titledPane);
+        hBox1.setSpacing(30.0);
+    
+        // Création du "font"
+        ToggleGroup toggleGroup = new ToggleGroup();
+            
+        RadioButton redButton = new RadioButton("Rouge");
+        RadioButton greeButton = new RadioButton("Vert");
+        RadioButton blueButton = new RadioButton("Bleu");
 
-        // Création fond
-        VBox vBoxFont = new VBox(10);
-        String[] arrayFont = { "Rouge", "Vert", "Bleu" };
+        redButton.setToggleGroup(toggleGroup);
+        greeButton.setToggleGroup(toggleGroup);
+        blueButton.setToggleGroup(toggleGroup);
 
-        for (String optionFont : arrayFont) {
-            RadioButton radioButton = new RadioButton(optionFont);
-            radioButton.setSelected(false);
-            vBoxFont.getChildren().add(radioButton);
-        }
 
-        TitledPane titledPaneFont = new TitledPane("Font", vBoxFont);
+        VBox vBoxBackground = new VBox(10, redButton, greeButton, blueButton);
+        
+        TitledPane titledPaneFont = new TitledPane("Font", vBoxBackground);
         VBox finalFont = new VBox(10, titledPaneFont);
 
-        // Création de la couleur des caractères
+        //Mettre un fond de couleur au texte
+        redButton.setOnAction(event -> updateFont(labelResult, "red"));
+        greeButton.setOnAction(event -> updateFont(labelResult,"green"));
+        blueButton.setOnAction(event -> updateFont(labelResult, "blue"));
+        
+        // Création de "couleur des caractères"
         VBox sliderVBox = new VBox(rouge, redSlider, vert, greenSlider, bleu, blueSlider);
         TitledPane titledPaneCharColor = new TitledPane("Couleur des caractères", sliderVBox);
-
-        //Mettre à jour le laberrresult lorsque le texte est modifié
-        textField.textProperty().addListener((observable, oldValue, newValue) -> { labelResult.setText(newValue);});
-        // Mettre la couleur sur le texte avec les sliders
+        
+        //Mettre à jour le labelresult lorsque le texte est modifié
+        textField.textProperty().addListener((observable, oldValue, newValue) -> labelResult.setText(newValue));
+        
+        //Mettre la couleur sur le texte avec les sliders
         redSlider.valueProperty().addListener((observable, oldValue, newValue) -> updateColor(labelResult, redSlider, greenSlider, blueSlider));
         greenSlider.valueProperty().addListener((observable, oldValue, newValue) -> updateColor(labelResult, redSlider, greenSlider, blueSlider));
         blueSlider.valueProperty().addListener((observable, oldValue, newValue) -> updateColor(labelResult, redSlider, greenSlider, blueSlider));
-
-        // Création du camelcase
-        VBox vBoxCamelCase = new VBox(10);
-        String[] arrayCamelCase = {"Majuscule", "Minuscule"};
-
-        for (String camelCase : arrayCamelCase) {
-            RadioButton camelCaseBox = new RadioButton(camelCase);
-            camelCaseBox.setSelected(false);
-            vBoxCamelCase.getChildren().add(camelCaseBox);
-        }
-
-        TitledPane titledPaneCamel = new TitledPane("Casse", vBoxCamelCase);
-        VBox finalCamel = new VBox(10, titledPaneCamel);
-
-        // Vbox finale //finalCharColor
-        VBox allVBox = new VBox(10, finalVBox, finalFont, titledPaneCharColor, finalCamel);
-        TitledPane mainTitledPane = new TitledPane("CheckBox - Radiobutton - Slider", allVBox);
         
-        Scene scene = new Scene(mainTitledPane, 640, 600);
+        // Création de "Casse"
+        ToggleGroup toggleGroup2 = new ToggleGroup();
+
+        RadioButton upperButton = new RadioButton("Majuscule");
+        RadioButton lowerButton = new RadioButton("Minuscule");
+        VBox vBoxCamelCase = new VBox(10, upperButton, lowerButton);
+
+        upperButton.setToggleGroup(toggleGroup2);
+        lowerButton.setToggleGroup(toggleGroup2);
+      
+        //Mettre à jour le labelresult en majuscule ou minuscule
+        upperButton.setOnAction(event -> updateTextCase(textField, labelResult, true));
+        lowerButton.setOnAction(event -> updateTextCase(textField, labelResult, false));
+        
+        TitledPane titledPaneCamel = new TitledPane("Casse", vBoxCamelCase);
+        HBox hBox2 = new HBox(finalFont, titledPaneCharColor, titledPaneCamel);
+        hBox2.setSpacing(50.0);
+       
+        // Vbox finale 
+        VBox allVBox = new VBox(30, hBox1, hBox2);
+                        
+        Scene scene = new Scene(allVBox, 500, 300);
         stage.setScene(scene);
         stage.show();
     }
 
-    // Methode pour mettre à jour la couleur des textes saisis
+    //Methode pour mettre un fond de couleur au texte
+    private void updateFont(Label label, String color) {
+    
+        label.setStyle("-fx-background-color: " + color);
+    } 
+       
+    // Methode pour mettre à jour la couleur avec les sliders des textes saisis
     private void updateColor(Label label, Slider redSlider, Slider greenSlider, Slider blueSlider) {
         int rouge = (int) redSlider.getValue();
         int vert = (int) greenSlider.getValue();
@@ -108,5 +134,21 @@ public class App extends Application {
         String hexaColor = String.format("#%02x%02x%02x", rouge, vert, bleu);
         label.setStyle("-fx-text-fill:" + hexaColor);
     }
+
+    //Methode pour mettre en majuscule ou minuscule
+    private void updateTextCase(TextField textField, Label labelResult, boolean toUpperCase) {
+        String text = textField.getText();
+
+        if (toUpperCase) {
+
+            labelResult.setText(text.toUpperCase());
+
+        } else {
+
+            labelResult.setText(text.toLowerCase());
+        }
+    }
+
+    
 
 }
